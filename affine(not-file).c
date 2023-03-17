45af49ae23089ba2d2f870656cddce681e9786fa
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-03-17 21:48:17 +0600

    Update affine(not-file).c

--- affine(not-file).c
+++ affine(not-file).c
@@ -1,55 +1,55 @@
 #include<stdio.h>
 #include<string.h>
 
-char a[20], b[20];     // a[] array-র মধ্যে ciphertext জমা করা হবে, আর b[] এর মধ্যে deciphertext
+char a[20], b[20];     
 
 int mulInv(int k1)
 {
-    for(int i=1; i<26; i++)     // কোন number এর  multiplicative inverse হবে ১ থেকে ২৫ এর মধ্যে ঐ number, যার সাথে গুন করে 26 দিয়ে mod করলে answer 1 আসবে
+    for(int i=1; i<26; i++)     
     {
-        if((k1 * i) % 26 == 1)      // যদি এমন number পাই, তাহলে ঐটা রে return করে দেবো
+        if((k1 * i) % 26 == 1)      
             return i;
     }
-    return -1;                  // আর যদি কোন multiplicative inverse না পাই, তাহলে -1 return করে দেবো
+    return -1;                  
 }
 
-void encrypt(char p[], int k1, int k2)              // Ciphertext এর Formula টা ছিলো এমন: C = ((P * K1) + K2) % 26
-{                                                         // p[i] হচ্ছে lowercase character, char এর সাথে তো আর k1, k2 number গুন-যোগ করা যাবে না
-    for(int i=0; i<strlen(p); i++)                              // এইটা থেকে 'a' বাদ দিলে number পাবো, আর number এর সাথেই K1 গুন করা লাগবে আর K2 যোগ করা লাগবে, then mod করবো
-    {                                                               // আর lastly 'A' যোগ করে দিলে, এইটা আবার number থেকে uppercase character হয়ে যাবে
-        a[i] = (((p[i]-'a')*k1 + k2) % 26) + 'A';                       // By the way, আমরা কিন্তু ciphertext টা global array a[] তে store করছি, পরে এইটা কে c[] array তে দিয়ে দিবো
+void encrypt(char p[], int k1, int k2)              
+{                                                         
+    for(int i=0; i<strlen(p); i++)                              
+    {                                                              
+        a[i] = (((p[i]-'a')*k1 + k2) % 26) + 'A';                       
     }
 }
 
 void decrypt(char c[], int k1, int k2)
 {
-    int k1_inv = mulInv(k1);    // k1 এর multiplicative inverse টা mulInv() function দিয়ে বের করে k1_inv এ store করলাম
-                                                        // Deciphertext এর Formula টা ছিলো এমন: P = ((C - K2) * K1_INV) % 26
-    for(int i=0; i<strlen(c); i++)                           // c[i] হচ্ছে uppercase character, char এর সাথে তো আর k1, k2 number গুন-যোগ করা যাবে না
-    {                                                               // এইখানেও encrypt এর মতোন same কাজ করা লাগবে, কিন্তু আগেরবার  শুরুতেছোটা হাতের 'a' বাদ দিছি, আর এইবার বড় হাতের 'A' যোগ করবো
-        b[i] = (((c[i] + 'A' - k2) * k1_inv) % 26) + 'a';               // আর শেষে 'a' যোগ করলে number থেকে lowercase character হয়ে যাবে, আর এইবার কিন্তু b[] তে রাখছি
+    int k1_inv = mulInv(k1);    
+                                                        
+    for(int i=0; i<strlen(c); i++)                           
+    {                                                              
+        b[i] = (((c[i] + 'A' - k2) * k1_inv) % 26) + 'a';               
     }
 }
 
 int main(){
 
-    char p[20], c[20], d[20];       // p[] তে রাখবো, plaintext, c[] তে ciphertext আর d[] তে deciphertext
+    char p[20], c[20], d[20];       
     int k1, k2;
-    scanf("%s", p);     // input নিলাম plaintext
+    scanf("%s", p);     
 
-    scanf("%d %d",&k1, &k2);    // input নিলাম key ২টা
-    while(mulInv(k1) == -1)         // 1st key এর যদি multiplicative inverse না থাকে (-1 মানে নাই), তাহলে key গুলা আবারো input নেওয়া লাগবে
+    scanf("%d %d",&k1, &k2);    
+    while(mulInv(k1) == -1)         
     {
         printf("k1 does not have multiplicative inverse. Enter Key values again:\n");
-        scanf("%d %d",&k1, &k2); // যতক্ষণ ভুল key দেবে, ততক্ষণ input নিতে থাকবো
+        scanf("%d %d",&k1, &k2); 
     }
 
-    encrypt(p, k1, k2); // encrypt() function এ plaintext আর key ২টা pass করে দিলাম, এই function টা a[] array-র মধ্যে ciphertext জমা করে দেবে
-    strcpy(c, a);   // a[] তে যেই ciphertext জমা করছি, ওইটা c[] array-র মধ্যে copy করে ফেলবো
+    encrypt(p, k1, k2); 
+    strcpy(c, a);   
     printf("Ciphertext: %s\n", c);
 
-    decrypt(c, k1, k2); // decrypt() function এ ciphertext আর key ২টা pass করে দিলাম, এই function টা b[] array-র মধ্যে deciphertext জমা করে দেবে
-    strcpy(d, b);   // b[] তে যেই deciphertext জমা করছি, ওইটা d[] array-র মধ্যে copy করে ফেলবো
+    decrypt(c, k1, k2); 
+    strcpy(d, b);   
     printf("Deciphertext: %s\n", d);
 
     return 0;
